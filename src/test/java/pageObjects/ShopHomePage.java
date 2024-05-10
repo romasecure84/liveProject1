e029a9ce98c1286a8a10fdb9307e34f345cee72e
ROMA2 <romasecure84@yandex.ru>
2024-05-10 20:05:31 +0400

    ShopPage class yaradildi

---
+++ src/test/java/pageObjects/ShopHomePage.java
@@ -0,0 +1,55 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ShopHomePage {
+    public WebDriver driver;
+
+    By productOne = By.xpath("(//*[text()='Hummingbird printed t-shirt'])[1]");
+    By productTwo = By.xpath("(//*[text()='Hummingbird printed sweater'])[1]");
+    By productThree = By.linkText("The best is yet to come'...");
+    By productFour = By.linkText("The adventure begins Framed...");
+    By productFive = By.linkText("Today is a good day Framed...");
+    By productSix = By.xpath("(//*[text()='Mug The best is yet to come'])[1]");
+    By productSeven = By.linkText("Mug The adventure begins");
+    By productEight = By.xpath("(//*[text()='Mug Today is a good day'])");
+
+    public ShopHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getProductOne() {
+        return driver.findElement(productOne);
+    }
+
+    public WebElement getProductTwo() {
+        return driver.findElement(productTwo);
+    }
+
+    public WebElement getProductThree() {
+        return driver.findElement(productThree);
+    }
+
+    public WebElement getProductFour() {
+        return driver.findElement(productFour);
+    }
+
+    public WebElement getProductFive() {
+        return driver.findElement(productFive);
+    }
+
+    public WebElement getProductSix() {
+        return driver.findElement(productSix);
+    }
+
+    public WebElement getProductSeven() {
+        return driver.findElement(productSeven);
+    }
+
+    public WebElement getProductEight() {
+        return driver.findElement(productEight);
+    }
+
+}
