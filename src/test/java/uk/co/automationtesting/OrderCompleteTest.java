f119781a717a2f78552b95c027126bd99c3a1191
ROMA2 <romasecure84@yandex.ru>
2024-05-11 23:45:04 +0400

    test metoduna elaveler edildi edildi

--- src/test/java/uk/co/automationtesting/OrderCompleteTest.java
+++ src/test/java/uk/co/automationtesting/OrderCompleteTest.java
@@ -2,11 +2,11 @@ package uk.co.automationtesting;
 
 import base.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pageObjects.HomePage;
-import pageObjects.ShopHomePage;
+import pageObjects.*;
 
 import java.io.IOException;
 
@@ -17,17 +17,36 @@ public class OrderCompleteTest extends BasePage {
     @BeforeTest
     public void setUp() throws IOException {
         driver = getDriver();
-        driver.get("https://www.automationtesting.co.uk/");
+        driver.get(getUrl());
     }
 
     @Test
-    public void endToEndTest() throws InterruptedException {
+    public void endToEndTest() throws InterruptedException, IOException {
         HomePage homePage = new HomePage(driver);
+        homePage.getCookie().click();
         Thread.sleep(2000);
         homePage.getTestStoreLink().click();
 
         ShopHomePage shopHomePage = new ShopHomePage(driver);
         shopHomePage.getProductOne().click();
+
+        ShopProductPage shopProductPage = new ShopProductPage(driver);
+        Select option = new Select(shopProductPage.getSizeOption());
+        option.selectByVisibleText("M");
+        shopProductPage.getQuantityIncrease().click();
+        shopProductPage.getAddToCartButton().click();
+
+        ShopContentPanel shopContentPanel = new ShopContentPanel(driver);
+        shopContentPanel.getCheckoutButton().click();
+
+        ShoppingCart shoppingCart = new ShoppingCart(driver);
+        shoppingCart.getHavePromo().click();
+        shoppingCart.getPromoTextBox().sendKeys("20OFF");
+        shoppingCart.getPromoAddButton().click();
+        Thread.sleep(2000);
+        shoppingCart.getProceedToCheckButton().click();
+
+
     }
 
     @AfterTest
